567a536b797b31f48b5f4fc67cfa6804ccb888b8
martacamacho <martacamachovargas@gmail.com>
2021-04-03 17:25:41 +0200

    changed app.css

--- src/App.vue
+++ src/App.vue
@@ -20,25 +20,6 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style src="./styles/App.css">
 
-#nav {
-  padding: 0;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: white;
-}
 </style>
